1c6d731eea36e3ce2e84e079816acbb95c7050e4
Karim Zibari <kzibari@gmail.com>
2018-03-14 02:49:29 +0000

    First Commit (Workflow)

---
+++ Controllers/AssignmentsController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Workflow.Models;
+
+namespace Workflow.Controllers
+{
+    public class AssignmentsController : Controller
+    {
+        private Workflow2Entities db = new Workflow2Entities();
+
+        // GET: Assignments
+        public ActionResult Index(DateTime? byDate, string searchTerm = null, string status = null)
+        {
+            var assignments = db.Assignments.Include(a => a.Employee).Include(a => a.Task);
+            
+
+            //Check for search criteria: date, name/email, completed/outstanding
+
+           if(status=="Completed")
+                assignments = db.Assignments.Include(a => a.Employee).Include(a => a.Task).Where(item=>status==null|| item.Completed==true);
+           if ((status == "Outstanding"))
+                assignments = db.Assignments.Include(a => a.Employee).Include(a => a.Task).Where(item => status == null || item.Completed == false);
+
+            if (byDate != null)
+            {
+                //store total assignments in ViewBag
+                ViewBag.Total = assignments.ToList().OrderBy(item => item.DueDate).Where(item => byDate == null || item.DueDate == byDate).Count();
+                return View(assignments.ToList().OrderBy(item => item.DueDate).Where(item => byDate == null || item.DueDate == byDate));
+            }
+            else
+            {
+                //store total assignments in ViewBag
+                ViewBag.Total = assignments.ToList().OrderBy(item => item.DueDate).Where(item => searchTerm == null || item.Employee.Name.Contains(searchTerm) ||
+                                                                                                                     item.Employee.Email.Contains(searchTerm)).Count();
+
+                return View(assignments.ToList().OrderBy(item => item.DueDate).Where(item => searchTerm == null || item.Employee.Name.Contains(searchTerm) ||
+                                                                                                                   item.Employee.Email.Contains(searchTerm)));
+            }
+        }
+
+        // GET: Assignments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Assignment assignment = db.Assignments.Find(id);
+            if (assignment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(assignment);
+        }
+
+        // GET: Assignments/Create
+        public ActionResult Create()
+        {
+            ViewBag.EmployeeID = new SelectList(db.Employees, "EmployeeID", "Name");
+            ViewBag.TaskID = new SelectList(db.Tasks, "TaskID", "Title");
+            return View();
+        }
+
+        // POST: Assignments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AssignmentID,SetDate,DueDate,Completed,EmployeeID,TaskID,CompletionDate")] Assignment assignment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Assignments.Add(assignment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.EmployeeID = new SelectList(db.Employees, "EmployeeID", "Name", assignment.EmployeeID);
+            ViewBag.TaskID = new SelectList(db.Tasks, "TaskID", "Title", assignment.TaskID);
+            return View(assignment);
+        }
+
+        // GET: Assignments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Assignment assignment = db.Assignments.Find(id);
+            if (assignment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.EmployeeID = new SelectList(db.Employees, "EmployeeID", "Name", assignment.EmployeeID);
+            ViewBag.TaskID = new SelectList(db.Tasks, "TaskID", "Title", assignment.TaskID);
+            return View(assignment);
+        }
+
+        // POST: Assignments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AssignmentID,SetDate,DueDate,Completed,EmployeeID,TaskID,CompletionDate")] Assignment assignment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(assignment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.EmployeeID = new SelectList(db.Employees, "EmployeeID", "Name", assignment.EmployeeID);
+            ViewBag.TaskID = new SelectList(db.Tasks, "TaskID", "Title", assignment.TaskID);
+            return View(assignment);
+        }
+
+        // GET: Assignments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Assignment assignment = db.Assignments.Find(id);
+            if (assignment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(assignment);
+        }
+
+        // POST: Assignments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Assignment assignment = db.Assignments.Find(id);
+            db.Assignments.Remove(assignment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Workflow.Models;
+
+namespace Workflow.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Workflow2Entities _db = new Workflow2Entities();
+
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index(string email)
+        {
+            if (email!=null)
+            {
+                var model = from r in _db.Assignments.ToList()
+                            where (r.Completed == false && r.Employee.Email == email)
+                            select r;
+
+                return View("ViewOutstandingTasks", model);
+            }
+            else
+                return View();
+        }
+
+        [HttpPost]
+        public ActionResult Complete(int id, string email)
+        {
+            _db.Assignments.Find(id).Completed = true;
+            _db.Assignments.Find(id).CompletionDate = DateTime.Today;
+            _db.SaveChanges();
+            
+            return RedirectToAction("Index", new { email = email });
+            
+        }
+        [HttpPost]
+        public ActionResult Refer(int id, string email)
+        {
+            var var = from r in _db.Employees.ToList()
+                        where (r.Email != email)
+                        select r;
+            SelectList list = new SelectList(var, "EmployeeID", "Name");
+            ViewBag.Employees = list;
+            ViewBag.AssignmentID = Request["id"];
+            ViewBag.Referer = email;
+            return View("Refer");
+
+        }
+        [HttpPost]
+        public ActionResult Assign(int id)
+        {
+
+            _db.Assignments.Find(id).EmployeeID = Int32.Parse(Request.Form["Employees"]);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index", new { email = Request.Form["email"] });
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+}
